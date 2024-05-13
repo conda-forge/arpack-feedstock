9d3491e23ccbfea97d18491fc8584c641c57e07c
Min RK <benjaminrk@gmail.com>
2024-05-13 08:24:39 +0200

    set env for cross-compile openmpi
    ompi needs many environment variables
    
    and needs to be in the build environment for cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,17 @@ if [[ "$(echo $fortran_compiler_version | cut -d '.' -f 1)" -gt 9 ]]; then
   export FFLAGS="$FFLAGS -fallow-argument-mismatch"
 fi
 
+if [[ $mpi == "openmpi" ]]; then
+  export OMPI_ALLOW_RUN_AS_ROOT=1
+  export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+  export OMPI_MCA_rmaps_base_oversubscribe=1
+  export OMPI_MCA_plm=isolated
+  export OMPI_MCA_btl=tcp,self
+  export OMPI_MCA_btl_vader_single_copy_mechanism=none
+  export OMPI_CC=$CC
+  export OPAL_PREFIX=$PREFIX
+fi  
+
 for shared_libs in OFF ON
 do
   cmake ${CMAKE_ARGS} \
